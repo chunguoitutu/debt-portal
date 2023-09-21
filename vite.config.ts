9d0be1e967007d21f5334e4a4a88c437b35cb70d
Bao Nguyen <nguyenlegiabaoab@gmail.com>
2023-09-21 10:46:29 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: "~", replacement: path.resolve(__dirname, "src") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components/"),
+      },
+      { find: "src", replacement: path.resolve(__dirname, "src") },
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [react()],
+});
