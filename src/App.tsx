5f839f13d2457d5a9f6dbec11917caec5523de05
Mang Nguyen <mang@lotusoutsourcing.com>
2023-10-05 11:20:19 +0700

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import AppRoutes from './router'
+import * as React from "react";
 
-const App: React.FC = () => {
-  return (
-    <AppRoutes />
-  )
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setToken } from "./features/user/userSlice";
+import { RootState } from "./app/store";
+import AppRoutes from "./router";
+
+function App() {
+  const dispatch = useDispatch();
+  dispatch(setToken("kha"));
+  const token = useSelector((state: RootState) => state.user.accessToken);
+
+  return  <AppRoutes />
 }
 
-export default App
+export default App;
