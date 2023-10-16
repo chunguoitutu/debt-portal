67f89be06388d2a3fa40b7322e3c88b733085508
Bao Nguyen <nguyenlegiabaoab@gmail.com>
2023-10-16 15:23:18 +0700

    Add priority config

---
+++ src/app/utils/globalConfig.ts
@@ -0,0 +1,22 @@
+export const ROLE_PRIORITY = [
+  {
+    value: 1,
+    label: 'Full-Access',
+  },
+  {
+    value: 2,
+    label: 'Branch Manager',
+  },
+  {
+    value: 3,
+    label: 'Finance',
+  },
+  {
+    value: 4,
+    label: 'Agent',
+  },
+  {
+    value: 5,
+    label: 'Debt Collect',
+  },
+]
