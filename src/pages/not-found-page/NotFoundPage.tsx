5e3019f9f6ea6c18af608533116d39a6e2dca61f
Bao Nguyen <nguyenlegiabaoab@gmail.com>
2023-11-17 15:24:17 +0700

    chore

--- src/pages/not-found-page/NotFoundPage.tsx
+++ src/pages/not-found-page/NotFoundPage.tsx
@@ -1,13 +1,11 @@
 import {Link} from 'react-router-dom'
 import Button from '../../components/button/Button'
+import {toAbsoluteUrl} from 'src/_metronic/helpers'
 
 const NotFoundPage = () => {
-  const publicUrl = process.env.PUBLIC_URL
-  const notFoundSvgPath = `${publicUrl}/media/svg/not-found/NotFound.svg`
-
   return (
     <div className='d-flex flex-column align-items-center justify-content-center align-self-center h-100'>
-      <img src={notFoundSvgPath} alt='Not Found' />
+      <img src={toAbsoluteUrl('/media/svg/not-found/NotFound.svg')} alt='Not Found' />
       <div className='fs-1 fw-bold'>Oops!</div>
       <div className='mt-4 fs-3 fw-normal'>We're sorry.</div>
       <div className='mt-1 fs-3 fw-normal'>The page you requested was not found.</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
+    resolve: {
+      alias: [{find: 'src', replacement: path.resolve(__dirname, 'src')}],
+    },
     define: {
       __APP_ENV__: env.APP_ENV,
     },
