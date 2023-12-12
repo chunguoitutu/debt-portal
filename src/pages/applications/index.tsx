d37cace03253d51a13eed9d059388bdea5d2c98a
Mang Nguyen <mang@lotusoutsourcing.com>
2023-12-12 16:42:21 +0700

    feat: add feature bankruptcy for step 4: employment

--- src/pages/applications/index.tsx
+++ src/pages/applications/index.tsx
@@ -183,7 +183,7 @@ export const Applications = () => {
       }
 
       const blockAddress = formik?.values?.address_contact_info || []
-      const allFieldShow = item.config || []
+      const allFieldShow = item.config.filter((el) => !el.isHide) || []
       let totalField = allFieldShow.length
       let number = 0
 
@@ -446,7 +446,11 @@ export const Applications = () => {
       customer_no,
       job_type_id,
       interest,
+      bankrupt_plan,
+      bankrupted,
     } = values
+
+    console.log(formik.values)
 
     const addressList = address_contact_info
       .filter((item) => item.address_type_id)
@@ -509,6 +513,8 @@ export const Applications = () => {
         position,
         specialization,
         six_months_income: +six_months_income,
+        bankrupt_plan: bankrupt_plan ? 1 : 0,
+        bankrupted: bankrupted ? 1 : 0,
       },
       application: {
         ...(applicationId && applicationIdEdit ? {id: applicationId} : {}),
