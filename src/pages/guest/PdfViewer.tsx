242c501cf31fd2ea76c930d100481cf33ac4d8b1
Lê Phước Kha <kha@lotusoutsourcing.com>
2023-12-27 15:43:48 +0700

    update

--- src/pages/guest/PdfViewer.tsx
+++ src/pages/guest/PdfViewer.tsx
@@ -36,7 +36,7 @@ export const PdfViewer = ({base64}: any) => {
   }
 
   return (
-    <Worker workerUrl='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js'>
+    <Worker workerUrl='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'>
       {pdfUrl && <Viewer fileUrl={pdfUrl} defaultScale={1.7} plugins={[layoutPlugin]} />}
     </Worker>
   )
