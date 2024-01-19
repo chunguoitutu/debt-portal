607a3debabc2f6a9aa2954fbe0ff5f9d10bdd632
Mang Nguyen <mang@lotusoutsourcing.com>
2024-01-19 11:19:03 +0700

    feat: done layout of CPF in Customer Details

--- src/pages/borrowers/borrowers-details/Overview/CPF.tsx
+++ src/pages/borrowers/borrowers-details/Overview/CPF.tsx
@@ -1,23 +1,41 @@
 import {CustomerCPF} from '@/app/types/customer'
 import clsx from 'clsx'
 import {Fragment, useEffect, useState} from 'react'
-import carImg from '@/app/images/car.svg'
+import cpfImg from '@/app/images/cpf.svg'
 
 import {CPF_CONFIG} from './config'
-import {formatDate, formatMoney} from '@/app/utils'
+import {formatDate, formatMoney, parseJson} from '@/app/utils'
 import NoRecord from '@/components/no-records'
-
-type Props = {}
 
 const CPF = ({data}: any) => {
   const {cpf: cpfData} = data || {}
 
+  const dataCPF = cpfData?.[0]
+
   const [showCpf, setShowCpf] = useState<any[]>([])
 
+  const [dataParse, setDataParse] = useState<any>(null)
+
   useEffect(() => {
-    const idDefault = cpfData?.[0].id
-    setShowCpf([idDefault])
-  }, [cpfData])
+    const {amount, date, employer, month} = dataCPF
+    if (!amount || !date || !employer || !month) return
+
+    const dateParse = parseJson(date)
+    const amountParse = parseJson(amount)
+    const monthParse = parseJson(month)
+    const employerParse = parseJson(employer)
+
+    const newData = dateParse.map((item, i) => {
+      return {
+        date: item,
+        amount: amountParse[i],
+        month: monthParse[i],
+        employer: employerParse[i],
+      }
+    })
+
+    setDataParse(newData)
+  }, [dataCPF])
 
   function handleToggleViewCpf(id: number) {
     const isOpen = showCpf.includes(id)
@@ -29,10 +47,10 @@ const CPF = ({data}: any) => {
 
   return (
     <div className='d-flex flex-column gap-12px'>
-      {cpfData?.length ? (
-        cpfData.map((vehicle: CustomerCPF, i: number) => {
-          const counter = i + 1
-          const isActive = showCpf.includes(vehicle.id)
+      {dataParse?.length ? (
+        dataParse.map((cpf: CustomerCPF, i: number) => {
+          const counter = formatDate(cpf.date, 'MMM DD, YYYY')
+          const isActive = showCpf.includes(cpf.id)
 
           return (
             <div
@@ -40,16 +58,17 @@ const CPF = ({data}: any) => {
                 'd-flex flex-column gap-16px',
                 i !== 0 && 'pt-12px border-0 border-top-1 border-dashed border-gray-300',
               ])}
-              key={vehicle.id}
+              key={cpf.id}
             >
               {/* Header */}
               <div
                 className='d-flex align-items-center justify-content-between gap-24px cursor-pointer'
-                onClick={() => handleToggleViewCpf(vehicle.id)}
+                onClick={() => handleToggleViewCpf(cpf.id)}
+                key={cpf.date}
               >
-                <div className='d-flex align-items-center gap-4px'>
-                  <img src={carImg} />
-                  <span className='fs-16 fw-semibold'>Vehicle {counter}</span>
+                <div className='d-flex align-items-center gap-4px' key={i}>
+                  <img src={cpfImg} />
+                  <span className='fs-16 fw-semibold'>{counter}</span>
                 </div>
 
                 {isActive ? (
@@ -68,13 +87,13 @@ const CPF = ({data}: any) => {
 
               {/* Body */}
               {isActive && (
-                <div className='grid-2-column gap-12px'>
+                <div className='grid-2-column gap-12px' key={i + 1}>
                   {CPF_CONFIG.map((el, i) => {
-                    let value = vehicle[el.key]
+                    let value = cpf[el.key]
 
-                    if (el?.format === 'date') {
+                    if (el?.key === 'date') {
                       value = formatDate(value, 'MMM DD, YYYY')
-                    } else if (el?.format === 'money') {
+                    } else if (el?.key === 'amount') {
                       value = formatMoney(+value)
                     }
 
