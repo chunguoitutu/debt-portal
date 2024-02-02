43440ff741278a086d3336e5a8617e6e8056c39b
Lê Phước Kha <kha@lotusoutsourcing.com>
2024-02-02 14:54:59 +0700

    header portal

--- src/pages/my-loans/MapData.tsx
+++ src/pages/my-loans/MapData.tsx
@@ -9,14 +9,14 @@ type Props = {
 
 const MapData = ({data}: Props) => {
   return (
-    <div className='row gy-20px mt-20px'>
+    <div className='row gy-20px '>
       {data.map((el) => {
         return (
-          <div className='col-12 col-lg-4 col-sm-6'>
-            <div className='card w-100 p-20px'>
+          <div className='col-12 col-xl-4  col-sm-6'>
+            <div className='card w-100 loans-id-my-loans-portal cursor-pointer p-20px'>
               <div className='d-flex justify-content-between'>
                 <div className=''>
-                  <h1 className='fs-13 fw-normal test-gray-700 mb-8px p-0'> Loan ID</h1>
+                  <h1 className='fs-13 fw-normal test-gray-4b5675-style mb-4px p-0'> Loan ID</h1>
                   <p className='fs-16 fw-bold text-gray-900 mb-16px p-0'>{el?.loan_no}</p>
                 </div>
                 <div className='position-relative'>
@@ -31,16 +31,16 @@ const MapData = ({data}: Props) => {
                 </div>
               </div>
               <div className=''>
-                <h2 className='fs-13 fw-normal test-gray-700 mb-8px p-0'>Loan Amount</h2>
+                <h2 className='fs-13 fw-normal test-gray-4b5675-style mb-4px p-0'>Loan Amount</h2>
                 <p className='fs-16 fw-bold text-gray-900 mb-20px p-0'>
                   {formatMoney(Number(el?.loan_amount))}
                 </p>
-                <div className='d-flex flex-column flex-md-row gap-8px gap-md-0 justify-content-between align-items-start'>
-                  <div className='d-flex flex-column flex-md-row gap-8px gap-md- justify-content-center align-items-center gap-12px'>
+                <div className='d-flex flex-column flex-md-row gap-8px gap-md-0 justify-content-between align-items-start align-items-md-center '>
+                  <div className='d-flex flex-column flex-md-row gap-8px gap-xl-12px justify-content-center align-items-center'>
                     <div className='d-flex justify-content-center ps-12px ps-md-0  align-items-center  gap-8px'>
                       <Icons name={'DatePortal'} />
                       <p className=' m-0 p-0 fs-12 fw-normal test-gray-900'>
-                        {moment(el?.approval_date).format('MMM D, YYYY')}
+                        {moment(el?.approval_date).format('DD MMM, YYYY')}
                       </p>
                     </div>
                     <div className='d-flex justify-content-center align-items-center  gap-8px'>
@@ -56,7 +56,7 @@ const MapData = ({data}: Props) => {
                     className='d-flex justify-content-center  ps-12px ps-md-0 align-items-center gap-8px'
                     to={'/my-loans/details/1'}
                   >
-                    <p className='link-details-my-task p-0 m-0'>Details</p>{' '}
+                    <p className='link-details-my-task p-0 m-0 fs-13'>Details</p>{' '}
                     <Icons name={'ArrowLeft'} />
                   </Link>
                 </div>
