ca76d7eb327a80896237a91e6f1887fa42e17ea3
Mang Nguyen <mang@lotusoutsourcing.com>
2024-02-19 15:43:11 +0700

    chore

--- src/pages/application/NewApplicationPortal.tsx
+++ src/pages/application/NewApplicationPortal.tsx
@@ -5,6 +5,18 @@ import clsx from 'clsx'
 import './style.scss'
 import {Input} from '@/components/input'
 import {CREATE_APPLICATION_PORTAL_CONFIG} from './config'
+import TitleContainer from '@/components/title-container.tsx'
+
+const profileBreadCrumbs = {
+  title: 'New Application',
+  link: [
+    {
+      to: '/',
+      titleLink: 'Home',
+    },
+  ],
+  render: ['New Application'],
+}
 
 const NewApplicationPortal = () => {
   const {rows} = CREATE_APPLICATION_PORTAL_CONFIG
@@ -34,50 +46,53 @@ const NewApplicationPortal = () => {
     onSubmit: () => {},
   })
 
-  console.log(values, 1234)
-
   return (
-    <div className='container pt-20px pt-xxl-40px pt-xl-40px pt-lg-40px pt-md-40px ps-0 pe-0 new-application-portal padding-top-custom'>
-      <div className='card mb-20px padding-custom'>
-        <div className='p-20px'>
-          <h1 className='fs-20 fw-bold text-gray-900  mb-0'>Create New Application</h1>
-        </div>
-        <div className='d-flex p-20px'>
-          <div className='flex-row-fluid'>
-            <form className='row gx-20px gy-16px ' onSubmit={handleSubmit}>
-              {rows
-                .filter((data) => !!data.infoCreateEdit)
-                .map((row) => {
-                  const {infoCreateEdit, name, key} = row
-                  const {typeComponent, column, className, columnMobile} = infoCreateEdit || {}
+    <>
+      <TitleContainer data={profileBreadCrumbs} />
+      <div className='container pt-20px pt-xxl-40px pt-xl-40px pt-lg-40px pt-md-40px ps-0 pe-0 new-application-portal padding-top-custom'>
+        <div className='card mb-20px padding-custom'>
+          <div className='p-20px'>
+            <h1 className='fs-20 fw-bold text-gray-900  mb-0'>Create New Application</h1>
+          </div>
+          <div className='d-flex p-20px'>
+            <div className='flex-row-fluid'>
+              <form className='row gx-20px gy-16px ' onSubmit={handleSubmit}>
+                {rows
+                  .filter((data) => !!data.infoCreateEdit)
+                  .map((row) => {
+                    const {infoCreateEdit, name, key} = row
+                    const {typeComponent, column, className, columnMobile} = infoCreateEdit || {}
 
-                  const columnClass = isMobile ? `col-${columnMobile || 12}` : `col-${column || 12}`
-                  const finalClassName = clsx([columnClass, 'mt-4', className])
+                    const columnClass = isMobile
+                      ? `col-${columnMobile || 12}`
+                      : `col-${column || 12}`
+                    const finalClassName = clsx([columnClass, 'mt-4', className])
 
-                  if (typeComponent === 'input') {
-                    return (
-                      <div className={finalClassName} key={key}>
-                        <Input
-                          required={!!infoCreateEdit.required}
-                          label={name}
-                          onBlur={handleBlur}
-                          name={key}
-                          value={values[key] || ''}
-                          type={infoCreateEdit.type}
-                          onChange={handleChange}
-                          noThereAreCommas={false}
-                          touched={!!touched[key]}
-                        />
-                      </div>
-                    )
-                  }
-                  return null
-                })}
-            </form>
+                    if (typeComponent === 'input') {
+                      return (
+                        <div className={finalClassName} key={key}>
+                          <Input
+                            required={!!infoCreateEdit.required}
+                            label={name}
+                            onBlur={handleBlur}
+                            name={key}
+                            value={values[key] || ''}
+                            type={infoCreateEdit.type}
+                            onChange={handleChange}
+                            noThereAreCommas={false}
+                            touched={!!touched[key]}
+                          />
+                        </div>
+                      )
+                    }
+                    return null
+                  })}
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
