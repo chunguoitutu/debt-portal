b2ef30ce6d42ed0cb7ac4a2c8807a60a6582c615
Mang Nguyen <mang@lotusoutsourcing.com>
2024-03-07 11:23:03 +0700

    chore

--- src/components/debt-title/index.tsx
+++ src/components/debt-title/index.tsx
@@ -2,7 +2,6 @@ import React, {FC} from 'react'
 import mainLogo from '@/app/images/logo-debt.svg'
 import {getFullName} from '@/app/utils'
 import {useAuth} from '@/app/context/AuthContext'
-import './style.scss'
 
 interface Props {
   title: string
@@ -14,7 +13,14 @@ const DebtTile: FC<Props> = ({title, className}) => {
 
   return (
     <div
-      className={`card d-flex flex-row align-items-center justify-content-between gap-4px position-sticky page-debt-title title-remove-border-card top-0 ${className}`}
+      className={`card d-flex flex-row align-items-center justify-content-between gap-4px position-sticky top-0 ${className}`}
+      style={{
+        borderRadius: '0px',
+        borderBottom: '1p solid #DBDFE9',
+        zIndex: 999,
+        borderLeft: 0,
+        borderRight: 0,
+      }}
     >
       <div className='d-flex flex-column p-12px'>
         <div className='fw-bold fs-18 text-gray-900'>{title}</div>
