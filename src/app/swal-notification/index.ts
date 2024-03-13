5cd17a93f841ba19945b05d3ed10810e6d796435
Thiện Thuần <thuan@lotusoutsourcing.com>
2024-03-13 09:30:42 +0700

    fix: fix popup swal

--- src/app/swal-notification/index.ts
+++ src/app/swal-notification/index.ts
@@ -18,9 +18,9 @@ export const swalConfirm = Swal.mixin({
   focusCancel: true,
   customClass: {
     htmlContainer: 'fs-14',
-    cancelButton: 'btn btn-lg order-0 btn-secondary fs-14 m-0 flex-grow-1',
-    confirmButton: 'order-1 btn btn-lg btn-danger fs-14 m-0 flex-grow-1',
-    actions: 'd-flex justify-content-center w-100 mt-20px gap-16px',
+    cancelButton: 'btn btn-lg order-0 btn-secondary fs-14 m-0 w-100',
+    confirmButton: 'order-1 btn btn-lg btn-danger fs-14 m-0 w-100',
+    actions: 'w-100 mt-20px gap-16px',
   },
 })
 
@@ -35,6 +35,6 @@ export const swalConfirmCancel = Swal.mixin({
     htmlContainer: 'fs-3',
     cancelButton: 'btn btn-lg order-0 btn-secondary fs-6',
     confirmButton: 'order-1 btn btn-lg btn-danger fs-6',
-    actions: 'd-flex justify-content-center w-100 ',
+    actions: 'w-100',
   },
 })
